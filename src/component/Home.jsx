14f925448009352200da91ea1916b11ca715f8ce
Ygor Garofalo <igorgarofalo@gmail.com>
2024-03-16 15:04:45 +0100

    fixes css

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const Home = () => {
-   return <div>Home</div>;
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2, // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+  };
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <div>
+            <h4>Latest Articles</h4>
+          </div>
+          <div></div>
+          <div></div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Home;
