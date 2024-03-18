fb768ed48be4dd6e727b03082d3b907e69e88c5c
IsuruFerna <isurumadushan.ferna@gmail.com>
2024-03-18 23:19:23 +0100

    adds localstorage hook and can create posts

--- src/fetchFunctions/index.js
+++ src/fetchFunctions/index.js
@@ -1,99 +1,128 @@
+import { TOKEN, useLocalStorage } from "../hooks/useLocalStorage";
+
 export const getAllCategories = () => {
-  return fetch("http://localhost:3035/posts/categories")
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      } else {
-        throw new Error("Error in fetching categories");
-      }
-    })
-    .then((data) => {
-      return data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+   return fetch("http://localhost:3035/posts/categories")
+      .then((res) => {
+         if (res.ok) {
+            return res.json();
+         } else {
+            throw new Error("Error in fetching categories");
+         }
+      })
+      .then((data) => {
+         return data;
+      })
+      .catch((err) => {
+         console.log(err);
+      });
 };
 
 export const getAllApprovedArticles = (page, size, order) => {
-  if (!page) {
-    page = 0;
-  }
-  if (!size) {
-    size = 10;
-  }
-  if (!order) {
-    order = "id";
-  }
+   if (!page) {
+      page = 0;
+   }
+   if (!size) {
+      size = 10;
+   }
+   if (!order) {
+      order = "id";
+   }
 
-  return fetch(
-    "http://localhost:3035/posts/all?page=" +
-      page +
-      "&size=" +
-      size +
-      "&order=" +
-      order
-  )
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      } else {
-        throw new Error("Error in fetching posts");
-      }
-    })
-    .then((data) => {
-      return data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+   return fetch(
+      "http://localhost:3035/posts/all?page=" +
+         page +
+         "&size=" +
+         size +
+         "&order=" +
+         order
+   )
+      .then((res) => {
+         if (res.ok) {
+            return res.json();
+         } else {
+            throw new Error("Error in fetching posts");
+         }
+      })
+      .then((data) => {
+         return data;
+      })
+      .catch((err) => {
+         console.log(err);
+      });
 };
 
 export const registerUser = (payload) => {
-  return fetch("http://localhost:3035/auth/register", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  })
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      } else {
-        throw new Error("Error in registering user");
-      }
-    })
-    .then((data) => {
-      console.log("registrazione ok");
-      return data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+   return fetch("http://localhost:3035/auth/register", {
+      method: "POST",
+      headers: {
+         "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+   })
+      .then((res) => {
+         if (res.ok) {
+            return res.json();
+         } else {
+            throw new Error("Error in registering user");
+         }
+      })
+      .then((data) => {
+         console.log("registrazione ok");
+         return data;
+      })
+      .catch((err) => {
+         console.log(err);
+      });
 };
 
 export const loginUser = (payload) => {
-  return fetch("http://localhost:3035/auth/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  })
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      } else {
-        throw new Error("Error in login user");
+   return fetch("http://localhost:3035/auth/login", {
+      method: "POST",
+      headers: {
+         "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+   })
+      .then((res) => {
+         if (res.ok) {
+            return res.json();
+         } else {
+            throw new Error("Error in login user");
+         }
+      })
+      .then((data) => {
+         console.log("login ok");
+         //  localStorage.setItem("token", data.token);
+
+         const { setItem } = useLocalStorage(TOKEN);
+         setItem(data.token);
+
+         return data;
+      })
+      .catch((err) => {
+         console.log(err);
+      });
+};
+
+export const postContent = async (postData, token) => {
+   try {
+      console.log("this is token from ls: ", token);
+      const response = await fetch(
+         import.meta.env.VITE_APP_BE_URL + "/posts/new",
+         {
+            method: "POST",
+            body: JSON.stringify(postData),
+            headers: {
+               Authorization: "Bearer " + token,
+               "Content-Type": "application/json",
+            },
+         }
+      );
+
+      if (response.ok) {
+         console.log("this is the reposne and post went fine!");
       }
-    })
-    .then((data) => {
-      console.log("login ok");
-      localStorage.setItem("token", data.token);
-      return data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+   } catch (error) {
+      console.log(error);
+   }
 };
--- src/page/CreateContentPage.jsx
+++ src/page/CreateContentPage.jsx
@@ -1,41 +1,77 @@
 import { Container } from "react-bootstrap";
 import AddCoverPhoto from "../component/post/AddCoverPhoto";
+import { useState } from "react";
+import { postContent } from "../fetchFunctions";
+import { TOKEN, useLocalStorage } from "../hooks/useLocalStorage";
 
 const CreateContentPage = () => {
+   const [postData, setPostData] = useState({
+      title: "title",
+      name: "bob",
+      category: "tech",
+      content: "",
+      tags: Array.from(["tech"]),
+   });
+
+   // gets token from localStorage
+   const { getItem } = useLocalStorage(TOKEN);
+
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      postContent(postData, getItem());
+      setPostData({
+         title: "",
+         name: "",
+         category: "",
+         content: "",
+         tags: "",
+      });
+   };
+
+   const handleInput = (e) => {
+      setPostData({
+         ...postData,
+         [e.target.name]: e.target.value,
+      });
+   };
+
    return (
       <Container>
          <AddCoverPhoto />
-         <div className="col-span-full">
-            <label
-               htmlFor="about"
-               className="block text-sm font-medium leading-6 text-gray-900"
-            >
-               Description
-            </label>
-            <div className="mt-2">
-               <textarea
-                  id="about"
-                  name="about"
-                  rows="10"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-500 sm:text-sm sm:leading-6"
-               ></textarea>
+         <form onSubmit={handleSubmit}>
+            <div className="col-span-full">
+               <label
+                  htmlFor="content"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+               >
+                  Description
+               </label>
+               <div className="mt-2">
+                  <textarea
+                     id="content"
+                     name="content"
+                     onChange={handleInput}
+                     rows="10"
+                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-500 sm:text-sm sm:leading-6"
+                  ></textarea>
+               </div>
             </div>
-         </div>
 
-         <div className="mt-6 flex items-center justify-end gap-x-6">
-            <button
-               type="button"
-               className="text-sm font-semibold leading-6 text-gray-900"
-            >
-               Cancel
-            </button>
-            <button
-               type="submit"
-               className="rounded-md bg-yellow-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-yellow-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-500"
-            >
-               Post
-            </button>
-         </div>
+            <div className="mt-6 flex items-center justify-end gap-x-6">
+               <button
+                  type="button"
+                  className="text-sm font-semibold leading-6 text-gray-900"
+               >
+                  Cancel
+               </button>
+               <button
+                  type="submit"
+                  className="rounded-md bg-yellow-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-yellow-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-500"
+               >
+                  Post
+               </button>
+            </div>
+         </form>
       </Container>
    );
 };
